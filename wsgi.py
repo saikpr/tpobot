b83775d57d928b51f86153b4b1703b038ce83a30
Sainyam Kapoor <sainyamkapoor@yahoo.com>
2016-07-29 10:00:38 +0530

    added dummy wsgi code

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+
+from flaskapp import app as application
+
+#
+# Below for testing only
+#
